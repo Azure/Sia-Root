ab3386d7fe01fb378c7f5dd6ed69251be74d7289
Philip Dimitratos <pdimit@microsoft.com>
2017-10-26 15:53:53 -0700

    Moved tests from Gateway to Sia-Root

---
+++ Domain/Sia.Shared.Tests/Validation/ThrowIfTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sia.Shared.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sia.Shared.Tests.Validation
+{
+    [TestClass]
+    public class ThrowIfTests
+    {
+        [TestMethod]
+        public void Null_StaticMethod_WhenObjectIsNotNull_ReturnsObject()
+        {
+            var input = new Object();
+
+            var result = ThrowIf.Null(input, nameof(input));
+
+            Assert.AreSame(input, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Null_StaticMethod_WhenObjectIsNull_ThrowsArgumentNullException()
+        {
+            object input = null;
+
+            var result = ThrowIf.Null(input, nameof(input));
+
+            //expect exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NullOrWhiteSpace_StaticMethod_WhenInputIsNull_ThrowsArgumentException()
+        {
+            string input = null;
+
+            var result = ThrowIf.NullOrWhiteSpace(input, nameof(input));
+
+            //Expect exception
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void NullOrWhiteSpace_StaticMethod_WhenInputIsOnlyWhitespace_ThrowsArgumentException()
+        {
+            string input = "    ";
+
+            var result = ThrowIf.NullOrWhiteSpace(input, nameof(input));
+
+            //Expect exception
+        }
+
+        [TestMethod]
+        public void NullOrWhiteSpace_StaticMethod_WhenInputStringWithAnyNonWhitespace_ReturnsString()
+        {
+            string input = " .  ";
+
+            var result = ThrowIf.NullOrWhiteSpace(input, nameof(input));
+
+            Assert.AreSame(input, result);
+        }
+    }
+}
