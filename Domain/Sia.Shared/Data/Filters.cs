8499baff0112b2a88d2bb69fbacfd075ab6fc4e4
Philip Dimitratos <pdimit@microsoft.com>
2017-11-21 16:10:15 -0800

    Added base class for filters

---
+++ Domain/Sia.Shared/Data/Filters.cs
@@ -0,0 +1,22 @@
+﻿using Sia.Shared.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sia.Shared.Data
+{
+    public abstract class Filters<T>
+    {
+        public abstract IQueryable<T> Filter(IQueryable<T> source);
+    }
+}
+
+namespace System.Linq
+{
+    public static class FilterExtensions
+    {
+        public static IQueryable<T> WithFilter<T>(this IQueryable<T> source, Filters<T> filter)
+            => filter.Filter(source);
+    }
+}
