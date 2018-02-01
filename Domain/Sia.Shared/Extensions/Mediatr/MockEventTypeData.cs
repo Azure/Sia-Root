0da8f370ab2f5bf0e9bc825825882f4885c6282b
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-02-01 11:02:23 -0800

    mock class for generatemock in shortcircuits

---
+++ Domain/Sia.Shared/Extensions/Mediatr/MockEventTypeData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sia.Shared.Extensions.Mediatr
+{
+    public class MockEventTypeData
+    {
+        public bool IsMock { get; } = true;
+    }
+}
