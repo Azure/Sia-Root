2b5b2b874c0218d5d4481ed4f04f5da00da97c80
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2018-02-06 10:45:58 -0800

    moved relevant tests & mocks to shared

---
+++ Domain/Sia.Shared.Tests/TestDoubles/StubController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;  
+using Sia.Shared.Authentication;
+using Sia.Shared.Controllers;
+
+namespace Sia.Shared.Tests.TestDoubles
+{
+
+    public class StubController : BaseController
+    {
+        protected new readonly IMediator _mediator;
+        protected new readonly DummyAzureActiveDirectoryAuthenticationInfo _authConfig;
+        protected new readonly IUrlHelper _urlHelper;
+
+        public StubController(IMediator mediator,
+            DummyAzureActiveDirectoryAuthenticationInfo authConfig,
+            IUrlHelper urlHelper) : base(mediator, authConfig, urlHelper)
+        {
+            _mediator = mediator;
+            _authConfig = authConfig;
+            _urlHelper = urlHelper;
+        }
+    }
+}
