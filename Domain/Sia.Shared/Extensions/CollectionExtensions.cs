6f7db69f04be1e9f316dcc66e6ca51eece040c95
Philip Dimitratos <pdimit@microsoft.com>
2018-03-05 16:04:23 -0800

    Altering filters to allow for bidirectional matching (single event matching list of filters)

---
+++ Domain/Sia.Shared/Extensions/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Collections.Concurrent
+{
+    public static class ConcurrentCollectionExtensions
+    {
+        /// <summary>
+        /// Minor wrapper around AddOrUpdate inteded for use when the passed in value should
+        /// always override the existing value in the dictionary.
+        /// </summary>
+        /// <returns>The final value in the concurrent dictionary (which may not be the passed in expected final value)</returns>
+        public static TValue Upsert<TKey, TValue>(
+            this ConcurrentDictionary<TKey, TValue> dictionary,
+            TKey keyToUpdate,
+            TValue intendedFinalValue
+        ) => dictionary.AddOrUpdate(keyToUpdate, intendedFinalValue, (key, value) => intendedFinalValue);
+    }
+}
