1af7202242b329206cc58d2534687b12e7a94809
pdimitratos <p.dimitratos@gmail.com>
2018-04-18 12:00:02 -0700

    Added helpers to ensure consistent format that comply with pragma (#36)

---
+++ Domain/Sia.Core/Extensions/FormattingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace System
+{
+    public static class NumberFormattingExtensions
+    {
+        private const string integerFormat = "D";
+        public static string ToPathTokenString(this long toConvert)
+            => toConvert.ToString(integerFormat, CultureInfo.InvariantCulture);
+    }
+
+    public static class DateTimeFormattingExtensions
+    {
+        private const string UniversalSortableFormat = "u";
+        public static string ToPathTokenString(this DateTime toConvert)
+            => toConvert.ToString(UniversalSortableFormat, CultureInfo.InvariantCulture);
+    }
+}
