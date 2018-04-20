1f77aa10d197a381e88ca772fdf8d23999e23493
Philip Dimitratos <pdimit@microsoft.com>
2018-04-20 16:05:11 -0700

    work in progress

--- Domain/Sia.Core/Extensions/CollectionExtensions.cs
+++ Domain/Sia.Core/Extensions/CollectionExtensions.cs
@@ -9,6 +9,8 @@ namespace System.Collections.Concurrent
         /// <summary>
         /// Minor wrapper around AddOrUpdate inteded for use when the passed in value should
         /// always override the existing value in the dictionary.
+        /// 
+        /// WARNING: This has a high risk of causing race conditions when multiple threads are upserting simultaneously
         /// </summary>
         /// <returns>The final value in the concurrent dictionary (which may not be the passed in expected final value)</returns>
         public static TValue Upsert<TKey, TValue>(
@@ -16,5 +18,24 @@ namespace System.Collections.Concurrent
             TKey keyToUpdate,
             TValue intendedFinalValue
         ) => dictionary.AddOrUpdate(keyToUpdate, intendedFinalValue, (key, value) => intendedFinalValue);
+    }
+}
+
+namespace System.Collections.Generic
+{
+    public static class CollectionExtensions
+    {
+        public static IDictionary<string, TValue> ToDictionary<TValue>(this IEnumerable<KeyValuePair<string, TValue>> entries)
+        {
+            var toReturn = new Dictionary<string, TValue>(StringComparer.InvariantCultureIgnoreCase);
+            foreach (var entry in entries)
+            {
+                if(!toReturn.ContainsKey(entry.Key))
+                {
+                    toReturn.Add(entry.Key, entry.Value);
+                }
+            }
+            return toReturn;
+        }
     }
 }
